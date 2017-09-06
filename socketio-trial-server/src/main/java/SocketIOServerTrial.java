efc65b79cf3faf012def8cbd2516dfdc714004c3
morel jerome <jem@morels-MacBook-Pro.local>
2017-09-06 13:54:48 +0200

    trials

--- socketio-trial-server/src/main/java/SocketIOServerTrial.java
+++ socketio-trial-server/src/main/java/SocketIOServerTrial.java
@@ -6,22 +6,28 @@ import com.corundumstudio.socketio.listener.ConnectListener;
 import com.corundumstudio.socketio.listener.DataListener;
 import com.corundumstudio.socketio.listener.DisconnectListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SocketIOServerTrial {
     public static void main(String[] args) throws Exception{
         Configuration configuration = new Configuration();
         configuration.setHostname("localhost");
         configuration.setPort(8082);
+        final List<SocketIOClient> socketsClient= new ArrayList<>();
         final SocketIOServer socketIOServer=new SocketIOServer(configuration);
         socketIOServer.addConnectListener(new ConnectListener() {
             public void onConnect(SocketIOClient socketIOClient) {
                 System.out.println("client connected");
+                socketsClient.add(socketIOClient);
+                socketsClient.forEach(s -> s.sendEvent("messages","this is amount of clients connected:"+socketsClient.size()));
                 socketIOClient.sendEvent("messages","hello world!");
             }
         });
         socketIOServer.addEventListener("messages", String.class, new DataListener<String>() {
             public void onData(SocketIOClient socketIOClient, String o, AckRequest ackRequest) throws Exception {
                 System.out.println("this is the received object:"+o);
-                socketIOClient.disconnect();
+//                socketIOClient.disconnect();
             }
         });
         socketIOServer.addDisconnectListener(new DisconnectListener() {
@@ -29,8 +35,9 @@ public class SocketIOServerTrial {
                 System.out.println("client is not connected anymore!");
             }
         });
-
+        socketIOServer.getAllClients()
         socketIOServer.start();
+
         Thread.sleep(3600000);
         socketIOServer.stop();
     }
